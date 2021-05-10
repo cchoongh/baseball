7de49a02d62cf066128776ae7b3db1c76d2cdc90
bibi <bibi6666667@kakao.com>
2021-05-10 17:09:46 +0900

    feat : 점수 반환을 위한 ScoreDTO 클래스 추가

---
+++ be/src/main/java/com/team18/baseball/dto/ScoreDTO.java
@@ -0,0 +1,16 @@
+package com.team18.baseball.dto;
+
+public class ScoreDTO {
+
+//    private final boolean isUserHomeTeam;
+//    private final boolean isUserBatting;
+    //halfinning에 담긴 score를 넣어 보내 준다.
+    // fieldingScoreList =
+    // battingScoreList
+    // fieldingTotal
+    // battingTotal
+
+
+
+
+}
