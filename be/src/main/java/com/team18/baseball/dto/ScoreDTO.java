67fe5cf57855c0e73297ed4b3acd37e0f3f2e91f
bibi <bibi6666667@kakao.com>
2021-05-10 20:20:47 +0900

    feat : ScoreDTO 필드 및 생성자 추가

--- be/src/main/java/com/team18/baseball/dto/ScoreDTO.java
+++ be/src/main/java/com/team18/baseball/dto/ScoreDTO.java
@@ -1,16 +1,30 @@
 package com.team18.baseball.dto;
 
+import java.util.List;
+
 public class ScoreDTO {
 
-//    private final boolean isUserHomeTeam;
-//    private final boolean isUserBatting;
+    private boolean isUserHomeTeam;
+    private boolean isUserBatting;
     //halfinning에 담긴 score를 넣어 보내 준다.
-    // fieldingScoreList =
-    // battingScoreList
-    // fieldingTotal
-    // battingTotal
+    private List<Integer> fieldingScore;
+    private List<Integer> battingScore;
+    private int fieldingTotal;
+    private int battingTotal;
 
+    private ScoreDTO(boolean isUserHomeTeam, boolean isUserBatting, List<Integer> fieldingScore,
+                    List<Integer> battingScore, int fieldingTotal, int battingTotal) {
+        this.isUserHomeTeam = isUserHomeTeam;
+        this.isUserBatting = isUserBatting;
+        this.fieldingScore = fieldingScore;
+        this.battingScore = battingScore;
+        this.fieldingTotal = fieldingTotal;
+        this.battingTotal = battingTotal;
+    }
 
-
+    public static ScoreDTO create(boolean isUserHomeTeam, boolean isUserBatting,  List<Integer> fieldingScore,
+                                  List<Integer> battingScore, int fieldingTotal, int battingTotal) {
+        return new ScoreDTO(isUserHomeTeam, isUserBatting, fieldingScore, battingScore, fieldingTotal, battingTotal);
+    }
 
 }
