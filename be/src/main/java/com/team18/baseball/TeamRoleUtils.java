52fcdd0229441acfc9a10ee6271cee8ead0f214d
bong6981 <kbk5190@naver.com>
2021-05-13 11:08:15 +0900

    fix : 계산 오류 수정
    - 홀수 짝수 이닝을 계산하기 위한 연산 오류 수정

--- be/src/main/java/com/team18/baseball/TeamRoleUtils.java
+++ be/src/main/java/com/team18/baseball/TeamRoleUtils.java
@@ -5,7 +5,8 @@ import com.team18.baseball.entity.game.TeamType;
 
 public class TeamRoleUtils {
     public static TeamRole checkTeamRole( TeamType teamType, int halfInningIndex) {
-        if((halfInningIndex / 2) == 1 ) {
+        if((halfInningIndex % 2) == 1 ) {
+            System.out.println(teamType == TeamType.HOME);
             if(teamType == TeamType.HOME) {
                 return TeamRole.FIELDING;
             }
