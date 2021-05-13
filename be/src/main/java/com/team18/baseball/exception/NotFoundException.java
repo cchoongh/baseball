e715bb918ee490589617cbe92834bbb282753e26
bong6981 <kbk5190@naver.com>
2021-05-13 15:54:43 +0900

    feat: NotFound 커스텀예외 추가

---
+++ be/src/main/java/com/team18/baseball/exception/NotFoundException.java
@@ -0,0 +1,5 @@
+package com.team18.baseball.exception;
+
+public class NotfoundExeption extends RuntimeException {
+    public NotfoundExeption
+}
---
+++ be/src/main/java/com/team18/baseball/exceptionHandler/GlobalExceptionHandler.java
@@ -0,0 +1,2 @@
+package com.team18.baseball.exceptionHandler;public class GlobalExceptionHandler {
+}
